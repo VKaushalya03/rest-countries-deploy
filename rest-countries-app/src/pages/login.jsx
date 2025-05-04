dcb2705426e417da16dccb9d12f04d386448e833
Vishwa Kaushalya <165917431+VKaushalya03@users.noreply.github.com>
2025-05-04 15:49:27 +0530

    Update login.jsx

--- rest-countries-app/src/pages/login.jsx
+++ rest-countries-app/src/pages/login.jsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://rest-countries-deploy.vercel.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
