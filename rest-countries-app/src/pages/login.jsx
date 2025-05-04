cb400b3b112cfa867ca91ab032ad902e1fcc025f
Vishwa Kaushalya <165917431+VKaushalya03@users.noreply.github.com>
2025-05-04 16:30:09 +0530

    Update login.jsx

--- rest-countries-app/src/pages/login.jsx
+++ rest-countries-app/src/pages/login.jsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
     }
 
     try {
-      const response = await fetch("https://rest-countries-deploy.vercel.app/api/login", {
+      const response = await fetch("https://rest-countries-deploy.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
